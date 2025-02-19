b0e00b8930ee2eaa24234f3ba39315d3fd390d0b
Abid <trooprasheed@gmail.com>
2025-02-19 14:38:59 -0800

    Change header icon color from teal to blue

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
                 {/* Islamic-inspired crescent icon */}
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 text-teal-500"
+                    className="h-6 w-6 text-blue-500"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
