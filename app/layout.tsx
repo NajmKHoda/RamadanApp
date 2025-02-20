c814fab95fb38e05851a5f19b8e103bccb43da3b
Abid <trooprasheed@gmail.com>
2025-02-19 19:27:03 -0800

    Add favicon and web manifest for improved branding and user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,13 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-    title: "UCLA MSA Ramadan Schedule | Powered by AMIN",
-    description: "Ramadan schedule for UCLA Muslim Student Association",
+    title: "Ramadan App",
+    description: "Ramadan prayer times",
+    icons: {
+        icon: "/favicon/favicon.ico",
+        shortcut: "/favicon/favicon-16x16.png",
+        apple: "/favicon/apple-touch-icon.png",
+    },
 };
 
 export default function RootLayout({
